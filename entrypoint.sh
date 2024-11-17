1228fc3077c4a3daff953fc10f0020c32d7666f2
MarcoRBosco <marcorbosco@gmail.com>
2024-11-17 19:01:38 +0000

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ CONFIGURATION=$4
 cd /docker-action
 echo "Creating a docker image with MPLAB X version ${MPLABX_VERSION} and XC8 version ${XC8_VERSION}"
 
+ls -la $(pwd)
+
 # Here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
 docker build \
