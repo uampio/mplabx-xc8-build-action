9549068b73525d5e717ffad91f9685dd22b8aded
MarcoRBosco <marcorbosco@gmail.com>
2025-02-03 11:48:08 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,34 +14,44 @@ echo "Building $PROJECT with configuration $CONFIGURATION"
 
 set -xe
 
-# Download and install MPLAB X IDE
-wget -q --referer="https://www.microchip.com/en-us/tools-resources/develop/mplab-x-ide" 
-    -O /tmp/MPLABX-v${MPLABX_VERSION}-linux-installer.tar 
-    https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/MPLABX-v${MPLABX_VERSION}-linux-installer.tar && 
-    cd /tmp && 
-    tar -xf MPLABX-v${MPLABX_VERSION}-linux-installer.tar && 
-    mv "MPLABX-v${MPLABX_VERSION}-linux-installer.sh" mplabx && 
-    chmod +x mplabx && 
-    sudo ./mplabx -- --unattendedmodeui none --mode unattended --ipe 0 --collectInfo 0 --installdir /opt/mplabx --16bitmcu 0 --32bitmcu 1 --othermcu 0 && 
-    rm mplabx
+# Download and install MPLABX
+wget -q --referer="https://www.microchip.com/en-us/tools-resources/develop/mplab-x-ide" \
+    -O /tmp/MPLABX-v${MPLABX_VERSION}-linux-installer.tar \
+    https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/MPLABX-v${MPLABX_VERSION}-linux-installer.tar
+
+cd /tmp
+
+tar -xf MPLABX-v${MPLABX_VERSION}-linux-installer.tar
+mv "MPLABX-v${MPLABX_VERSION}-linux-installer.sh" mplabx
+chmod +x mplabx
+
+sudo ./mplabx -- --unattendedmodeui none --mode unattended --ipe 0 --collectInfo 0 --installdir /opt/mplabx --16bitmcu 0 --32bitmcu 1 --othermcu 0
+
+rm mplabx
 
 # Download and install XC8 compiler
-wget -nv -O /tmp/xc8 "https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/xc8-v${XC8_VERSION}-full-install-linux-x64-installer.run" && 
-    chmod +x /tmp/xc8 && 
-    sudo /tmp/xc8 --mode unattended --unattendedmodeui none --netservername localhost --LicenseType FreeMode --prefix "/opt/microchip/xc8/v${XC8_VERSION}" && 
-    rm /tmp/xc8
+wget -nv -O /tmp/xc8 "https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/xc8-v${XC8_VERSION}-full-install-linux-x64-installer.run"
+chmod +x /tmp/xc8
+
+sudo /tmp/xc8 --mode unattended --unattendedmodeui none --netservername localhost --LicenseType FreeMode --prefix "/opt/microchip/xc8/v${XC8_VERSION}"
+
+rm /tmp/xc8
 
 # Install DFPs
-if [ -n "$DFP_PACKS" ]; then 
-    echo "Installing DFPs: $DFP_PACKS"; 
-    sudo chmod +x /opt/mplabx/mplab_platform/bin/packmanagercli.sh; 
-    for pack in $(echo "$DFP_PACKS" | tr "," "\n"); do 
-        pack_name=$(echo "$pack" | cut -d '=' -f 1); 
-        pack_version=$(echo "$pack" | cut -d '=' -f 2); 
-        echo "Installing package: $pack_name (Version: $pack_version)"; 
-        sudo /opt/mplabx/mplab_platform/bin/packmanagercli.sh --install-pack "$pack_name" --version "$pack_version" > /dev/null 2>&1; 
-    done; 
+if [ -n "$DFP_PACKS" ]; then
+    echo "Installing DFPs: $DFP_PACKS"
+    sudo chmod +x /opt/mplabx/mplab_platform/bin/packmanagercli.sh
+    
+    IFS=',' read -ra PACK_ARRAY <<< "$DFP_PACKS"
+    for pack in "${PACK_ARRAY[@]}"; do
+        pack_name=$(echo "$pack" | cut -d '=' -f 1)
+        pack_version=$(echo "$pack" | cut -d '=' -f 2)
+        echo "Installing package: $pack_name (Version: $pack_version)"
+        sudo /opt/mplabx/mplab_platform/bin/packmanagercli.sh --install-pack "$pack_name" --version "$pack_version" > /dev/null 2>&1
+    done
 fi
+
+echo "MPLABX and XC8 installation complete."
 
 # Generate project makefiles
 echo "Generating makefiles"
